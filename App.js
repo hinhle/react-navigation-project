5ebb6e19badb35dd1b5a0e23f658755096ad0559
Hinh <levhinhvh@gmail.com>
2020-09-18 11:13:29 +0700

    event with header button

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import  MainComponent  from "./components/MainComponent";
-import  DetailComponent  from "./components/DetailComponent";
-import  ThirdComponent  from "./components/ThirdComponent";
-import {NavigationContainer} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MainComponent from "./components/MainComponent";
+import DetailComponent from "./components/DetailComponent";
+import ThirdComponent from "./components/ThirdComponent";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Button from "react-native-button";
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={MainComponent} />
+        <Stack.Screen
+          name="Home"
+          component={MainComponent}
+          options={{
+            title: "My home",
+            headerTitleAlign: "center",
+            headerStyle: {
+              backgroundColor: "#fff",
+            },
+            headerTintColor: "#000",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+            headerRight: () => (
+              <Button containerStyle={{ padding: 10 }}>Save</Button>
+            ),
+          }}
+        />
         <Stack.Screen name="Detail" component={DetailComponent} />
         <Stack.Screen name="Third" component={ThirdComponent} />
       </Stack.Navigator>
@@ -23,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,51 +1,97 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import Button from 'react-native-button';
+import React, { Component } from "react";
+import { View, Text, ActivityIndicator } from "react-native";
+import Button from "react-native-button";
 import { MainScreen, DetailScreen, ThirdScreen } from "../screenNames";
 
 export default class MainComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
-
+  _onSave() {
+    console.log("You pressed Save");
+    console.log("navigation params", this.props.route.params);
+    if (this.props.route.params.isSaving == true) return;
+    this.props.navigation.setParams({ isSaving: true });
+    setInterval(() => {
+      this.props.navigation.setParams({ isSaving: false });
+    }, 3000);
+  }
   render() {
-    console.log(`this.props = ${JSON.stringify(this.props)}`);
+    console.log(this.props);
     const { navigation } = this.props;
+    navigation.setOptions({
+      title: "My home",
+      headerTitleAlign: "center",
+      headerStyle: {
+        backgroundColor: "#fff",
+      },
+      headerTintColor: "#000",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+      headerRight: () => (
+        <Button containerStyle={{ padding: 10 }} onPress={() => this._onSave()}>
+          Save
+        </Button>
+      ),
+    });
     let dataSendToDetail = {
-        name: 'Star Wars',
-        releaseYear: 1977
-    }
-    return (
-      <View style={{
-          flex: 1,
-          backgroundColor: 'dodgerblue',
-          alignItems: 'center',
-          justifyContent: 'center'
-      }}>
-        <Text style={{ fontWeight: 'bold', fontSize: 22, color: 'white'}}>
+      name: "Star Wars",
+      releaseYear: 1977,
+    };
+    let mainView =
+      this.props.route.params && this.props.route.params.isSaving == true ? (
+        <ActivityIndicator />
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "dodgerblue",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ fontWeight: "bold", fontSize: 22, color: "white" }}>
             This is the main screen
-        </Text>
-        <Button
-            containerStyle={{ padding: 10, margin: 20, width: 200, borderRadius: 10, backgroundColor: 'darkviolet'}}
-            style={{ fontSize: 18, color: 'white'}}
+          </Text>
+          <Button
+            containerStyle={{
+              padding: 10,
+              margin: 20,
+              width: 200,
+              borderRadius: 10,
+              backgroundColor: "darkviolet",
+            }}
+            style={{ fontSize: 18, color: "white" }}
             onPress={() => {
-                navigation.navigate("Detail", dataSendToDetail);
-                //this.props.navigate(DetailScreen);
-            }}>
-                Navigate to detail
-            </Button>
-            <Button
-            containerStyle={{ padding: 10, margin: 20, width: 200, borderRadius: 10, backgroundColor: 'darkviolet'}}
-            style={{ fontSize: 18, color: 'white'}}
+              navigation.navigate("Detail", dataSendToDetail);
+              //this.props.navigate(DetailScreen);
+            }}
+          >
+            Navigate to detail
+          </Button>
+          <Button
+            containerStyle={{
+              padding: 10,
+              margin: 20,
+              width: 200,
+              borderRadius: 10,
+              backgroundColor: "darkviolet",
+            }}
+            style={{ fontSize: 18, color: "white" }}
             onPress={() => {
-                navigation.navigate("Third");
-                //this.props.navigate(DetailScreen);
-            }}>
-                Navigate to third
-            </Button>
-      </View>
-    );
+              navigation.navigate("Third");
+              //this.props.navigate(DetailScreen);
+            }}
+          >
+            Navigate to third
+          </Button>
+        </View>
+      );
+    return mainView;
+  }
+  componentDidMount() {
+    this.props.navigation.setParams({ isSaving: false });
   }
 }
